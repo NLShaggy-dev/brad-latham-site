5836c0686586a25478a082f5030165d910a1981b
nlshaggy <viking2012df@gmail.com>
2023-01-04 22:22:39 -0700

    Initial commit

---
+++ test/brad-latham-site.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BradLathamSite from '../lib/brad-latham-site-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/brad-latham-site-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BradLathamSite.BradLathamSiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
